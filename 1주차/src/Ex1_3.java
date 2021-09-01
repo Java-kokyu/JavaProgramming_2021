33b6dc040ed39776b15733713adcbfcdbf41fdb2
Minji Kim <85613861+Java-kokyu@users.noreply.github.com>
2021-09-01 16:34:25 +0900

    고급자바프로그래밍
    ~#1.3

--- 1주차/src/Ex1_3.java
+++ 1주차/src/Ex1_3.java
@@ -5,15 +5,16 @@ class MyFirstFrame extends JFrame {
 	MyFirstFrame() {
 		this.setTitle("Minji's First Java Window");
 		this.setSize(300, 200);
-		this.setLocation(200, 200);	
+		this.setLocation(200, 200);
 	}
 }
+
 public class Ex1_3 {
 
 	public static void main(String[] args) {
-			JFrame frame = new MyFirstFrame();			
-			
-			frame.setVisible(true);
+		JFrame frame = new MyFirstFrame();
+
+		frame.setVisible(true);
 
 	}
 
